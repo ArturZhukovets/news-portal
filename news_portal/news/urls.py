0839e105f4d15e93b8340ecd849a749abf5d46e0
Zhukovec Artur <bnlvdn7483@gmail.com>
2022-05-20 11:20:20 +0300

    Creating a new url for news form

--- news_portal/news/urls.py
+++ news_portal/news/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.news_home, name='news-home'),
+    path('create', views.news_create, name='create'),
     ]
--- news_portal/news/views.py
+++ news_portal/news/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-
+from .models import NewsArticles
 # Create your views here.
 
 
 def news_home(request):
-    return render(request, 'news/news_home.html')
+    news = NewsArticles.objects.order_by('-date')
+    context = {'news': news}
+    return render(request, 'news/news_home.html', context)
+
+
+def news_create(request):
+    return render(request, 'news/create.html')
